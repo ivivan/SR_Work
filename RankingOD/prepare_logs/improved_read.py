43dc5bea4d5bd3002ad33db2f872fb77dd630366
ivanbuaa@hotmail.com <ivanbuaa@hotmail.com>
2017-03-15 21:32:32 +1000

    improved if

--- RankingOD/prepare_logs/improved_read.py
+++ RankingOD/prepare_logs/improved_read.py
@@ -6,6 +6,7 @@ from RankingOD.prepare_logs import output_logs as olog
 import multiprocessing
 import timeit
 
+
 def multiprocess_log(filepath):
     result_together = []
     multiprocessing.freeze_support()
@@ -19,16 +20,9 @@ def multiprocess_log(filepath):
             if entry.name.endswith(".zip"):
                 files.append(entry.path)
 
-
     for f in files:
         for log in list_log_in_zip(f):
-            results.append(pool.apply_async(process_each_log_in_zip, [log,f]))
-
-
-
-
-
-
+            results.append(pool.apply_async(process_each_log_in_zip, [log, f]))
 
     pool.close()
     pool.join()
@@ -46,17 +40,7 @@ def multiprocess_log(filepath):
     result_together = [result.get() for result in results]
     processed_results = [num for elem in result_together for num in elem]
 
-
     print(processed_results)
-
-
-    # print(result_queue.get())
-
-
-            # print(logfiles)
-
-    # processed_results = [num for elem in result_together for num in elem]
-    # olog.output_group(processed_results,files[0])
 
 
 def list_log_in_zip(filepath):
@@ -70,13 +54,69 @@ def list_log_in_zip(filepath):
     return files
 
 
+# def process_each_log_in_zip(logfile,f):
+#     eachxml = ''
+#     ip_value = ''
+#     ts_value = ''
+#     odpairs = []
+#     withinxml = False
+#     getxmlhead = False
+#     findip = False
+#     findts = False
 
-def process_each_log_in_zip(logfile,f):
+#     response_start_pattern = re.compile(b'^<JourneyPlanningResponse')
+#     response_stop_pattern = re.compile(b'^</JourneyPlanningResponse>')
+#     ip_pattern = re.compile(b'^IP')
+#     timestamp_pattern = re.compile(b'^TimeStamp')
+#     ipaddr_pattern = re.compile(
+#         r'((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d))))')
+#     tsvalue_pattern = re.compile(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}:\d{4}')
+
+
+#     with zipfile.ZipFile(f).open(logfile) as log:
+#         for line in log:
+#             if line:
+#                 if not findip and re.match(ip_pattern, line) is not None:
+#                     ip_value = line.decode('utf-8')
+#                     findip = True
+#                 if not findts and re.match(timestamp_pattern, line) is not None:
+#                     ts_value = line.decode('utf-8')
+#                     findts = True
+#                 if re.match(response_start_pattern, line) is not None:
+#                     eachxml += line.decode('utf-8')
+#                     withinxml = True
+#                     getxmlhead = True
+#                 elif re.match(response_stop_pattern, line) is not None:
+#                     if getxmlhead:
+#                         eachxml += line.decode('utf-8')
+#                         withinxml = False
+#                         getxmlhead = False
+#                         print(logfile)
+#                         eachod = plog.parse_response_xml_lxml(eachxml)
+#                         [e.insert(3, re.search(ipaddr_pattern, ip_value).group(0) if re.search(ipaddr_pattern,
+#                                                                                                ip_value) else '0.0.0.0')
+#                          for e in eachod]
+#                         [e.insert(4,
+#                                   re.search(tsvalue_pattern, ts_value).group(0) if re.search(
+#                                       tsvalue_pattern, ts_value) else '29990101')
+#                          for e in eachod]
+
+#                         odpairs.extend(eachod)
+#                         eachxml = ''
+#                         findip = False
+#                         findts = False
+#                 elif withinxml:
+#                     eachxml += line.decode('utf-8')
+
+#     zipfile.ZipFile(f).close()
+#     return odpairs
+
+
+def process_each_log_in_zip(logfile, f):
     eachxml = ''
     ip_value = ''
     ts_value = ''
     odpairs = []
-    withinxml = False
     getxmlhead = False
     findip = False
     findts = False
@@ -89,44 +129,40 @@ def process_each_log_in_zip(logfile,f):
         r'((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d))))')
     tsvalue_pattern = re.compile(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}:\d{4}')
 
-
     with zipfile.ZipFile(f).open(logfile) as log:
         for line in log:
             if line:
-                if not findip and re.match(ip_pattern, line) is not None:
-                    ip_value = line.decode('utf-8')
-                    findip = True
-                if not findts and re.match(timestamp_pattern, line) is not None:
-                    ts_value = line.decode('utf-8')
-                    findts = True
-                if re.match(response_start_pattern, line) is not None:
-                    eachxml += line.decode('utf-8')
-                    withinxml = True
-                    getxmlhead = True
-                elif re.match(response_stop_pattern, line) is not None:
-                    if getxmlhead:
+                if not getxmlhead:
+                    if not findip and re.match(ip_pattern, line) is not None:
+                        ip_value = line.decode('utf-8')
+                        findip = True
+                    if not findts and re.match(timestamp_pattern, line) is not None:
+                        ts_value = line.decode('utf-8')
+                        findts = True
+                    if re.match(response_start_pattern, line) is not None:
                         eachxml += line.decode('utf-8')
-                        withinxml = False
-                        getxmlhead = False
-                        print(logfile)
-                        eachod = plog.parse_response_xml_lxml(eachxml)
-                        [e.insert(3, re.search(ipaddr_pattern, ip_value).group(0) if re.search(ipaddr_pattern,
-                                                                                               ip_value) else '0.0.0.0')
-                         for e in eachod]
-                        [e.insert(4,
-                                  re.search(tsvalue_pattern, ts_value).group(0) if re.search(
-                                      tsvalue_pattern, ts_value) else '29990101')
-                         for e in eachod]
+                        getxmlhead = True
+                elif re.match(response_stop_pattern, line) is not None:
+                    eachxml += line.decode('utf-8')
+                    getxmlhead = False
+                    print(logfile)
+                    eachod = plog.parse_response_xml_lxml(eachxml)
+                    [e.insert(3, re.search(ipaddr_pattern, ip_value).group(0) if re.search(ipaddr_pattern,
+                                                                                           ip_value) else '0.0.0.0')
+                     for e in eachod]
+                    [e.insert(4,
+                              re.search(tsvalue_pattern, ts_value).group(0) if re.search(
+                                  tsvalue_pattern, ts_value) else '29990101')
+                     for e in eachod]
 
-                        odpairs.extend(eachod)
-                        eachxml = ''
-                        findip = False
-                        findts = False
-                elif withinxml:
+                    odpairs.extend(eachod)
+                    eachxml = ''
+                else:
                     eachxml += line.decode('utf-8')
 
     zipfile.ZipFile(f).close()
     return odpairs
+
 
 def process_each_log(filepath):
     eachxml = ''
@@ -166,8 +202,10 @@ def process_each_log(filepath):
                                         withinxml = False
                                         getxmlhead = False
                                         print(file_name)
-                                        eachod = plog.parse_response_xml_lxml(eachxml)
-                                        [e.insert(3, re.search(ipaddr_pattern, ip_value).group(0) if re.search(ipaddr_pattern, ip_value) else '0.0.0.0') for e in eachod]
+                                        eachod = plog.parse_response_xml_lxml(
+                                            eachxml)
+                                        [e.insert(3, re.search(ipaddr_pattern, ip_value).group(0) if re.search(
+                                            ipaddr_pattern, ip_value) else '0.0.0.0') for e in eachod]
                                         [e.insert(4,
                                                   re.search(tsvalue_pattern, ts_value).group(0) if re.search(
                                                       tsvalue_pattern, ts_value) else '29990101')
@@ -179,9 +217,8 @@ def process_each_log(filepath):
                                     eachxml += line.decode('utf-8')
     z.close()
 
-
-    filedir,name = os.path.split(filepath)
-    name,ext = os.path.splitext(name)
+    filedir, name = os.path.split(filepath)
+    name, ext = os.path.splitext(name)
     outputfiledir = os.path.abspath(os.path.join(filedir, 'DataBase', name))
     if not os.path.exists(outputfiledir):
         os.makedirs(outputfiledir)
@@ -190,23 +227,12 @@ def process_each_log(filepath):
     resultforDB = olog.results_dataframe_all(odpairs)
     resultforDB.to_csv(csvfile, sep=',', encoding='utf-8')
 
-
     # return odpairs
-
-
-
-
 
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
     start = timeit.default_timer()
-    multiprocess_log(r'C:\Logs\ZIP')
+    multiprocess_log(r'/Users/Ivan/Downloads/TEST')
     stop = timeit.default_timer()
     print('Spent %s seconds processing one day log.' % str(stop - start))
-
-
-
-
-
-
