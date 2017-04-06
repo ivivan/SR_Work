4c4c028ae81b20640243624866e2306a93d81bac
ivivan <ivanbuaa@hotmail.com>
2017-04-06 14:26:15 +1000

    time series data format

--- RankingOD/analyse_logs/prepare_df.py
+++ RankingOD/analyse_logs/prepare_df.py
@@ -9,30 +9,28 @@ def read_csv(filepath):
 
 def results_dataframe_counts(odarray,filename):
     """Log info in dataframe, O, D, Count"""
-    title = ('Origin', 'Destination', 'Time', 'Distance', 'PrimaryServiceProviderCode')
+    title = ('Origin', 'Destination', 'Time', 'Distance', 'PrimaryServiceProviderCode', 'Date')
     df = pd.DataFrame(odarray, columns=list(title))
     group_df = df.groupby(['Origin', 'Destination']).size().reset_index(name='Count')
-    group_df['Date'] = filename  # add date column, date is just the filename
     sorted_df = group_df.sort_values(by='Count', ascending=0).reset_index(drop=True)
     return sorted_df
 
 
 def all_results_with_count(origin_df,filename):
     origin_df.drop('Time', axis=1, inplace=True)
-    count_df = results_dataframe_counts(origin_df)
+    count_df = results_dataframe_counts(origin_df, filename)
     all_df = pd.merge(origin_df, count_df,
               left_on=['Origin', 'Destination'],
               right_on=['Origin', 'Destination'],
               how='inner')
     result = all_df.groupby(['Origin', 'Destination']).first().reset_index()  # remove the duplicate rows and only keep the first value
-    result['Date'] = filename  # add date column, date is just the filename
     result.sort_values(['Date', 'Count'], ascending=[True, False], inplace=True)
     return result
 
 
 def results_dataframe_all(odarray):
     """Log info in dataframe, O, D, D P"""
-    title = ('Origin', 'Destination', 'Distance', 'PrimaryServiceProviderCode')
+    title = ('Origin', 'Destination', 'Distance', 'PrimaryServiceProviderCode', 'Date')
     df = pd.DataFrame(odarray, columns=list(title))
     return df
 
@@ -58,5 +56,5 @@ def output_df(filepath):
 
 
 if __name__ == '__main__':
-    output_df(r'C:\work\project\logprocess\join_logs\20170307\20170307.csv')
-    #output_df_all(r'C:\work\project\logprocess\join_logs\20170306\20170306.csv')
+    #output_df(r'C:\work\project\logprocess\join_logs\20170307\20170307.csv')
+    output_df_all(r'C:\work\project\logprocess\join_logs\20170306\20170306.csv')
